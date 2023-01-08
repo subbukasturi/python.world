54ab9d8c9090ed8a20d27b6b5042faeb935bdb6a
k.subramanyam <121307557+subbukasturi@users.noreply.github.com>
2023-01-09 00:02:29 +0300

    Add files via upload

---
+++ about.py
@@ -0,0 +1,42 @@
+'''programming is the process of creating aset of instructions that tell a cpmputer how to perfom atask
+coding is the process of transforming ideas,solutions and instructions into the langisge that the 
+computer can understand-that is bianry machine code.
+binary language-(0,1)
+compiler is a computer program that thanslate source code from a high level programing language to a
+lower level language.(totall code reed)
+compiler works source code - compiler - machine code or target code(0,1) - output.
+interpreter line by line code reed
+source code - interpreter - output.
+bite code is a pyc file.
+python is a programming language which high level,intrepreted,interactive and object oriented scripting
+language.
+python was developed by guido van rossum in 1991(netherlands).
+python features - dynamically typed,object oriented,large community support,open source,large standard
+library,platform independent,extensible and embeddable,high level languge.
+comments means-code explanesion thar are two types 1)single line(#),2)multi line(''' ''').
+variables are containers for storing data values.
+'''
+#a is variable name and = is assignment operator and 9 is value.
+#a=9
+#print(a)
+
+#a=b=3
+#print(a)
+#print(b)
+#print(a,b)
+
+#a=1,2,3,4,5
+#print(a)
+
+#variable location identifi
+#subbu=9
+#raji=5
+#print(id(subbu))
+
+#rules
+# a variable name must start with aletter or the underscore character.(_subbu)
+# a variable name cannot satrt with a number.
+# a variable name can only contain alpha-numeric characters and underscores(A-Z,0-9,and_).
+# a variable name are case-sensitive(name,Name and NAME are three different variables.)
+# the reserved words(keywords) cannot be used naming the variable.
+
